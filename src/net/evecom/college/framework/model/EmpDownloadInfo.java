1211ba071399cc7f03fabae09686149f86989a1d
Pa-gom <903418053@qq.com>
2016-12-12 20:55:10 +0800

    v5

---
+++ src/net/evecom/college/framework/model/EmpDownloadInfo.java
@@ -0,0 +1,10 @@
+package net.evecom.college.framework.model;
+
+import com.jfinal.plugin.activerecord.Model;
+
+/**
+ * Created by Ezreal on 2016/12/12.
+ */
+public class EmpDownloadInfo extends Model<EmpDownloadInfo> {
+
+}
